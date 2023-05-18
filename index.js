55c31bf5ff30f41dd2d6fcac48ff9f0e3fe13d9a
IgorLima <igorlimail2018il@gmail.com>
2023-05-18 11:28:25 -0300

    Ajustes Header
    Adicionando include no header,

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const PORT = process.env.PORT
 
 app.set("view engine", "ejs")
+app.set("head", "views/partials")
 
 app.use("/", require("./routes/helloworld").router)
 app.use("/", require("./routes/autarquia-routes").router)
