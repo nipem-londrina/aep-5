f9218a2cb2915dc0606b522e20fa282204063af8
erich-ika <erich.sabe.tudo@gmail.com>
2023-06-13 18:07:30 -0300

    Mudadas as rotas /autarquia para /a
    Meu objetivo é continuar com as rotas organizadas por tipo de usuário sem confundir o usuário com endereços grandes. Não é esquisito, você como autarquia, acessar o link "nipem.com/autarquia/cadastrar/campo_estagio"? agora seria "nipem.com/a/cadastrar/campo_estagio", omitindo a parte que vc ja sabe (você é a autarquia)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 app.use("/", require("./routes/hello_world").router)
-app.use("/autarquia", require("./routes/autarquia").router)
+app.use("/a", require("./routes/autarquia").router)
 app.use("/api/v1/campodeestagio", require("./routes/campo_estagio").router)
 
 app.listen(PORT, () => console.log(`Listening to http://localhost:${PORT}`))
