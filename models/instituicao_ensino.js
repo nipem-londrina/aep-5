098bf3f7cd9b4a5724875a6114a438f446669106
erich-ika <erich.sabe.tudo@gmail.com>
2023-06-16 16:45:00 -0300

    Corrigido erro ao cadastrar Insituições de Ensino
    ops

--- models/instituicao_ensino.js
+++ models/instituicao_ensino.js
@@ -2,7 +2,7 @@ const { connection } = require("../database/connection")
 
 class InstituicaoEnsino {
     async cadastrar(razaoSocial, cnpj, tipoDeInstituicao, nomeContato, telefone, email, endereco, senha) {
-        const [result] = connection.execute(
+        const [result] = await connection.execute(
             "INSERT INTO insituicaoDeEnsino (razaoSocial, cnpj, tipoDeInstituicao, nomeContato, telefone, endereco) VALUES (?,?,?,?,?,?)",
             [razaoSocial, cnpj, tipoDeInstituicao, nomeContato, telefone, endereco]
         )
